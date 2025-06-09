63bddfae75c9f5398aaf0874d339073fada14db5
Ravi Sharma <uvraviz26@gmail.com>
2025-06-09 22:15:03 +0530

    second commit completed models part

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,7 +1,52 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
+
+const  userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    age:{
+        type: Number,
+        required:true
+    },
+    email:{
+         type:String,
+         unique:true
+    },
+    mobile:{
+        type:Number,
+        required:true,
+        unique:true
+    },
+    adharId:{
+        type:Number,
+        required:true,
+        unique:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        enum:['voter','admin'],
+        default:'voter'
+    },
+    isVoted:{
+        type:Boolean,
+        default:false
+    }
 
 });
+const User=mongoose.model('User',userSchema);
+module.export=User;
 
-export const User = mongoose.model('User', userSchema);
+
+
+
+
--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,35 @@
 import express from 'express';
+const app = express();
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import userRoute from './routes/user.route.js';
+import bodyParser from 'body-parser'
+app.use(bodyParser.json());
 
-const app = express();
+
+
 dotenv.config();
 const port = process.env.PORT || 3000;
 const uri = process.env.MONGODB_URI;
 
 app.use(express.json());
 
-(async () => {
-  try {
-    await mongoose.connect(uri);
-    console.log("Connected to DataBase");
-  } catch (error) {
-    console.log(error);
-  }
-})();
+// (async () => {
+//   try {
+//     await mongoose.connect(uri);
+//     console.log("Connected to DataBase");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// })();
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Next Generation Voting System');
-});
-app.get('/users', userRoute);
+// app.get('/', (req, res) => {
+//   res.send('Welcome to the Next Generation Voting System');
+// });
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Sever listening on port ${port}`);
