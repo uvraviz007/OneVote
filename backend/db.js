adb00dce4f1d1f8f00ec2c62f635b249f21fcc77
Ravi Sharma <uvraviz26@gmail.com>
2025-07-01 22:40:13 +0530

    ignored

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // Define the MongoDB connection URL with fallback
-const mongoURL = process.env.MONGODB_URI || 'mongodb://localhost:27017/voting_machine';
+const mongoURL =  'mongodb+srv://phoenix:12345@cluster.r6mhes6.mongodb.net/';
 
 // Set up MongoDB connection with proper options
 const connectDB = async () => {
